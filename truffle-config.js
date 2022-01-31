8d41249f421e7df816c599912750c9d3b17c6a68
mgeffre10 <geffrejunior92@gmail.com>
2022-01-30 17:23:41 -0800

    Setting up truffle to connect to rinkeby and sample contract

--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const mnemonic = fs.readFileSync('.secret').toString().trim();
+const infuraID = fs.readFileSync('.infura').toString().trim();
 
 module.exports = {
   /**
@@ -35,6 +36,19 @@ module.exports = {
    */
 
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+     },
+     rinkeby: {
+         provider: function() { 
+           return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraID}`);
+         },
+         network_id: 4,
+         gas: 4500000,
+         gasPrice: 10000000000,
+     }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
